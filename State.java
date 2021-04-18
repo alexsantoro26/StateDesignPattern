b7f149adc8e493ad238cb95e749fa20698e03c8a
alexsantoro26 <77907258+alexsantoro26@users.noreply.github.com>
2021-04-17 21:12:25 -0400

    adding State Class

---
+++ State.java
@@ -0,0 +1,12 @@
+/**
+ * State Design Pattern
+ * Code written by Alex Santoro
+ */
+
+public interface State {
+     void pressHomeButton();
+     void pressNetflixButton();
+     void pressHuluButton();
+     void pressMovieButton();
+     void pressTVButton();
+}
